d9cf4258f40b8ce3fb1bb77306a81d7b1c8d8e97
nachog86 <gomezignacio86@gmail.com>
2023-07-16 05:52:38 -0300

    modificado el app.js y ver cuale s el error en CountryCard.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { LANDING_PAGE, HOME_PAGE, DETAIL_PAGE } from './utils/pathRoutes.js'; // Importar la nueva constante
+import { LANDING_PAGE, HOME_PAGE, DETAIL_PAGE } from './utils/pathRoutes.js';
 import Landing from './components/Landing/Landing.jsx';
 import Home from './components/Home/Home.jsx';
-import CountryDetailPage from './components/Detail/Detail.jsx'; // Asegúrate de que esta ruta sea correcta
+import CountryDetailPage from './components/Detail/Detail.jsx';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path={HOME_PAGE}>
+        <Route exact path={LANDING_PAGE}>
+          <Landing />
+        </Route>
+        <Route exact path={HOME_PAGE}>
           <Home />
         </Route>
         <Route path={DETAIL_PAGE}>
-          <CountryDetailPage /> {/* Nueva ruta para la página de detalles */}
-        </Route>
-        <Route path={LANDING_PAGE}>
-          <Landing />
+          <CountryDetailPage />
         </Route>
       </Switch>
     </Router>
@@ -25,3 +25,4 @@ function App() {
 }
 
 export default App;
+
--- client/src/components/Home/CountryCard.jsx
+++ client/src/components/Home/CountryCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import styles from '../Home/CountryCard.module.css';
 
 const CountryCard = ({ country }) => {
